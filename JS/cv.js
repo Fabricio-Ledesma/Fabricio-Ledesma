dea203ebb5053d9e7e31ade0368d618e13bf9ccb
Fabricio-Ledesma <ledesmafabricio29@gmail.com>
2023-03-01 16:53:02 -0300

    Se agrega el proyecto

---
+++ JS/cv.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById ('boton').onclick = function (){
+    console.log ("capturamos el evento click");
+decumemt.getElementById ("Agradecimiento").innerHTML= "texto de agradecimiento"
+
+}
